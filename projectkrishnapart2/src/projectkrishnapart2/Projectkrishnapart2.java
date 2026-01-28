6f5dcf695d4a309a1e39d8d14ea0c8f840efd76d
Krishna Patel <pate6047@sheridancollege.ca>
2026-01-28 15:28:17 -0500

    Add initial second time setup changes

---
+++ projectkrishnapart2/src/projectkrishnapart2/Projectkrishnapart2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectkrishnapart2;
+
+/**
+ *
+ * @author patel
+ */
+public class Projectkrishnapart2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello from krishna part2 ICE4 step 6");
+    }
+    
+}
